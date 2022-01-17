562a1387bfcce78d4af1fe74edeb5f410a827fb2
proietti-s <stefano.pro@docsity.com>
2022-01-17 14:55:45 +0100

    aggiunta box citazioni

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ document.addEventListener('DOMContentLoaded', function(){
     const container = document.querySelector('.container');
     const input_src = document.querySelector('.src_input');
     const btn_src = document.querySelector('.src_btn');
+    const modalBox = document.querySelector('.modal');
+    const modalContent = document.querySelector('.modal_content');
 
-    const base_url = "https://www.breakingbadapi.com/api/characters";
+    const base_url = "https://www.breakingbadapi.com/api";
 
     //////////////GET DATA FUNCTIONS/////////////////
     const getAllCharacters = async function(){
     
-        const base_res = await fetch(base_url);
+        const base_res = await fetch(`${base_url}/characters`);
         const base_data = await base_res.json();
         console.log(base_data);
         base_data.map(character => renderCharacters(character));
@@ -19,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
     const getCharacter = async function(id){
-        const char_url = `${base_url}` + `/${id}`;
+        const char_url = `${base_url}/characters/${id}`;
         const char_res = await fetch(char_url);
         const char_data = await char_res.json();
         char_data.map(character => console.log(character));
@@ -29,7 +31,7 @@ document.addEventListener('DOMContentLoaded', function(){
     const searchCharacter = async function(name){
         name = input_src.value;
 
-        const src_url = `${base_url}` + `?name=${name}`;
+        const src_url = `${base_url}/characters?name=${name}`;
         const src_res = await fetch(src_url);
         const src_data = await src_res.json();
         
@@ -38,6 +40,16 @@ document.addEventListener('DOMContentLoaded', function(){
             renderCharacters(character);
         });
         
+    }
+    
+    const getRandomQuoteByAuthor = async function(author){
+        const quotes_url = `${base_url}/quote/random?author=${author}`;
+        const quotes_res = await fetch(quotes_url);
+        const quotes_data = await quotes_res.json();
+        quotes_data.map(author => {
+            console.log(author)
+            renderQuote(author)
+        })
     }
 
 
@@ -62,9 +74,20 @@ document.addEventListener('DOMContentLoaded', function(){
         const details = characterEl.querySelector('.details');
         details.addEventListener('click', function(e){
             e.preventDefault()
-            console.log(data.char_id)
+            getRandomQuoteByAuthor(data.name);
+
+            modalBox.style.display = 'flex';
         })
 
+    }
+
+    const renderQuote = function(data){
+        const quotes_HTML = `
+            <h3 class="info_name">${data.author}</h3>
+            <p class="info_nick">${data.quote}</p>
+        `
+        modalContent.innerHTML = '';
+        modalContent.innerHTML = quotes_HTML;
     }
 
 
