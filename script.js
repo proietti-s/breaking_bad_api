2d1f74e1a37e84becadd0d79e7927bb650cb0889
proietti-s <stefano.pro@docsity.com>
2022-01-17 11:53:46 +0100

    aggiornamento fetch e modifiche css

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 document.addEventListener('DOMContentLoaded', function(){
 
     const container = document.querySelector('.container');
+    const input_src = document.querySelector('.src_input');
+    const btn_src = document.querySelector('.src_btn');
 
     const base_url = "https://www.breakingbadapi.com/api/characters";
 
@@ -12,8 +14,7 @@ document.addEventListener('DOMContentLoaded', function(){
         const base_res = await fetch(base_url);
         const base_data = await base_res.json();
         console.log(base_data);
-        for(const character of base_data)
-        renderCharacters(character)
+        base_data.map(character => renderCharacters(character));
         
     }
 
@@ -21,8 +22,24 @@ document.addEventListener('DOMContentLoaded', function(){
         const char_url = `${base_url}` + `/${id}`;
         const char_res = await fetch(char_url);
         const char_data = await char_res.json();
-        console.log(char_data)
+        char_data.map(character => console.log(character));
+
     }
+
+    const searchCharacter = async function(name){
+        name = input_src.value;
+
+        const src_url = `${base_url}` + `?name=${name}`;
+        const src_res = await fetch(src_url);
+        const src_data = await src_res.json();
+        
+        !name || src_data.length === 0 ? alert('Not Found') :
+        src_data.map(character => {
+            renderCharacters(character);
+        });
+        
+    }
+
 
     /////////////RENDER DATA FUNCTIONS///////////////
     const renderCharacters = function(data){
@@ -39,10 +56,27 @@ document.addEventListener('DOMContentLoaded', function(){
             </div>
         </div>
         `
+        
         characterEl.innerHTML = characterInnerHTML;
         container.appendChild(characterEl);
+        const details = characterEl.querySelector('.details');
+        details.addEventListener('click', function(e){
+            e.preventDefault()
+            console.log(data.char_id)
+        })
+
     }
 
-    getAllCharacters()
+
+    getAllCharacters();
+
+    
+
+    btn_src.addEventListener('click', function(e){
+        e.preventDefault()
+        container.innerHTML='';
+        searchCharacter();
+
+    });
 
 })
